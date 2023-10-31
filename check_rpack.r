ee9ab769b23e2bbcf2022fb4ff99081c7c19231a
Li Zhen <zhenlisme@gmail.com>
2023-10-31 12:06:50 +0100

    Delete check_rpack.r

--- check_rpack.r
+++
@@ -1,3 +0,0 @@
-
-tryCatch({library('seqinr')}, error=function(e){print("R package seqinr not installed!")})
-tryCatch({library('bedtoolsr')}, error=function(e){print("R package bedtoolsr not installed!")})
